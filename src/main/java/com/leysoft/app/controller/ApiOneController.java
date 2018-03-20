af5f7157f947d16bc90527a54cd132c1099b01a2
dabliuw22 <wleylop22@gmail.com>
2018-03-19 22:55:38 -0500

    Agregando archivos del proyecto

---
+++ src/main/java/com/leysoft/app/controller/ApiOneController.java
@@ -0,0 +1,19 @@
+package com.leysoft.app.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiOneController {
+	
+	@Value(value = "${service.message}")
+	private String message;
+	
+	@GetMapping(value = {"/"})
+	public ResponseEntity<String> message() {
+		return new ResponseEntity<String>(message, HttpStatus.OK);
+	}
+}
